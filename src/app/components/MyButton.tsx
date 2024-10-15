42bbad77b3cbe8b3f076ce5a0707bb65ec9eb845
Davi Araújo <davi.lucas350@gmail.com>
2024-10-15 09:52:18 -0300

    Aula 02 - Componentes

---
+++ src/app/components/MyButton.tsx
@@ -0,0 +1,13 @@
+export function MyButton({ onClick }: { onClick: () => void }) {
+    return (
+      <div className="mt-8">
+        <button
+          onClick={onClick}
+          className="px-4 py-2 bg-purple-800 text-white rounded-lg"
+        >
+          Clique aqui
+        </button>
+      </div>
+    );
+  }
+  
---
+++ src/app/components/MyTitle.tsx
@@ -0,0 +1,10 @@
+export function MyTittle() {
+    return (
+      <div className="mt-8">
+        <h1 className="text-4xl font-bold text-purple-800">
+          Título da Página
+        </h1>
+      </div>
+    );
+  }
+  
